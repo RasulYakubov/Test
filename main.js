5f27a4906b5220a5d5901c4813aaa11326813909
RasulYakubov <yakubov.rasul1996@yandex.ru>
2021-04-07 19:33:03 +0500

    localstorage handle functions included

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 function handleAddBtnClick() {
-    console.log('click')
+    var addDataContainer = document.getElementById('addData');
+    var inputElement = addDataContainer.firstElementChild;
+
+    var inputValue = inputElement.value;
+}
+
+function render(item) {
+    var span = document.createElement('span');
+    span.innerText = item;
+
+    document.getElementById('container').append(span);
+}
+
+function writeToLS(item) {
+    var array = readArrayFromLS();
+    array.push(item);
+
+    localStorage.setItem('items', JSON.stringify(array));
+}
+
+function readArrayFromLS() {
+    var response = localStorage.getItem('items');
+
+    if (response) {
+        response = JSON.parse(response);
+    } else {
+        response = [];
+    }
+
+    return response;
 }
 
 function handleWindowLoad() {
