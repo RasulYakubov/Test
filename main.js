6c1cd3e875e47bfe7a59c44966c238497b491b11
RasulYakubov <yakubov.rasul1996@yandex.ru>
2021-04-07 19:00:50 +0500

    handle window load and handle add btn click provided

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-
+function handleAddBtnClick() {
+    console.log('click')
+}
 
 function handleWindowLoad() {
-    
+    var addDataContainer = document.getElementById('addData');
+    var addBtn = addDataContainer.lastElementChild;
+
+    addBtn.addEventListener('click', handleAddBtnClick);
 }
 
 window.addEventListener('load', handleWindowLoad);
